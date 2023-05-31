53d3a4a094e97ad1f3a7717309d413c2544c2f5b
Kartik Agrawal <kartikagrawal4321@gmail.com>
2023-05-31 19:43:17 +0530

    changes

--- script.js
+++ script.js
@@ -1,68 +1,70 @@
 
 onkeyup = event => {
-    console.log(event.key);
+    // console.log(event.key);
     const k1 = event.key;
-    if (k1 == 'w') {
-        let play = document.getElementById('box-1');
-        document.getElementById('box-1').classList.add('nums');
-        play.addEventListener("click", playMusic());
-        setInterval(() => {
-            document.getElementById('box-1').classList.remove('nums');
-        }, 2000);
-    }
-    else if (k1 == 'a') {
-        let play = document.getElementById('box-2');
-        document.getElementById('box-2').classList.add('nums');
-        setInterval(() => {
-            document.getElementById('box-2').classList.remove('nums');
-        }, 2000);
+    switch (k1) {
+        case 'w':
+            let play = document.getElementById('box-1');
+            play.classList.add('nums');
+            setInterval(() => {
+                play.classList.remove('nums');
+            }, 2000);
+            play.addEventListener("keypress", playMusic());
+            break;
+        case 'a':
+            let play1 = document.getElementById('box-2');
+            play1.classList.add('nums');
+            setInterval(() => {
+                play1.classList.remove('nums');
+            }, 2000);
 
-        play.addEventListener("click", music2());
-    }
-    else if (k1 == 's') {
-        let play = document.getElementById('box-3');
-        document.getElementById('box-3').classList.add('nums');
-        setInterval(() => {
-            document.getElementById('box-3').classList.remove('nums');
-        }, 2000);
+            play1.addEventListener("keypress", music2());
+            break;
+        case 's':
+            let play2 = document.getElementById('box-3');
+            play2.classList.add('nums');
+            setInterval(() => {
+                play2.classList.remove('nums');
+            }, 2000);
 
-        play.addEventListener("click", music3());
-    }
-    else if (k1 == 'd') {
-        let play = document.getElementById('box-4');
-        document.getElementById('box-4').classList.add('nums');
-        setInterval(() => {
-            document.getElementById('box-4').classList.remove('nums');
-        }, 2000);
+            play2.addEventListener("keypress", music3());
+            break;
+        case 'd':
+            let play3 = document.getElementById('box-4');
+            play3.classList.add('nums');
+            setInterval(() => {
+                play3.classList.remove('nums');
+            }, 2000);
 
-        play.addEventListener("click", music2());
-    }
-    else if (k1 == 'j') {
-        let play = document.getElementById('box-5');
-        document.getElementById('box-5').classList.add('nums');
-        setInterval(() => {
-            document.getElementById('box-5').classList.remove('nums');
-        }, 2000);
+            play3.addEventListener("click", music2());
+            break;
+        case 'j':
+            let play4 = document.getElementById('box-5');
+            play4.classList.add('nums');
+            setInterval(() => {
+                play4.classList.remove('nums');
+            }, 2000);
 
-        play.addEventListener("click", playMusic());
-    }
-    else if (k1 == 'k') {
-        let play = document.getElementById('box-6');
-        document.getElementById('box-6').classList.add('nums');
-        setInterval(() => {
-            document.getElementById('box-6').classList.remove('nums');
-        }, 2000);
+            play4.addEventListener("keypress", playMusic());
+            break;
+        case 'k':
+            let play5 = document.getElementById('box-6');
+            play5.classList.add('nums');
+            setInterval(() => {
+                play5.classList.remove('nums');
+            }, 2000);
 
-        play.addEventListener("click", playMusic());
-    }
-    else if (k1 == 'l') {
-        let play = document.getElementById('box-7');
-        document.getElementById('box-7').classList.add('nums');
-        setInterval(() => {
-            document.getElementById('box-7').classList.remove('nums');
-        }, 2000);
+            play5.addEventListener("keypress", playMusic());
+            break;
+        case 'l':
+            let play6 = document.getElementById('box-7');
+            play6.classList.add('nums');
+            setInterval(() => {
+                play6.classList.remove('nums');
+            }, 2000);
 
-        play.addEventListener("click", music3());
+            play6.addEventListener("keypress", music3());
+            break;
     }
     function playMusic() {
         let audio = new Audio("music.mp3");
@@ -71,14 +73,14 @@ onkeyup = event => {
             audio.pause();
         }, 2000);
     }
-    function music2(){
+    function music2() {
         let audio = new Audio("musice2.mp3");
         audio.play();
         setInterval(() => {
             audio.pause();
         }, 1500);
     }
-    function music3(){
+    function music3() {
         let audio = new Audio("music3.mp3");
         audio.play();
         setInterval(() => {
